cf39795040a6bc14cc6910d1fd5df58d12cdcdea
thiagomiyaguchi <89756000+thiagomiyaguchi@users.noreply.github.com>
2024-01-04 19:27:32 -0300

    Adicionar arquivos de projeto.

---
+++ JsonParaTxt/Frase.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace JsonParaTxt;
+
+internal class Frase
+{
+    [JsonPropertyName("quote")]
+    public string Citacao { get; set; }
+
+    public void MostraFrase()
+    {
+        Console.WriteLine($"Frase Genial de Kanye West: {Citacao}");
+    }
+}
+
+
---
+++ JsonParaTxt/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+using JsonParaTxt;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    Console.Write("Pressione uma tecla para gerar um arquivo TXT com uma frase dita pelo Sr. Kanye West");
+    Console.ReadKey();
+    try
+    {
+        string frase = await client.GetStringAsync("https://api.kanye.rest");
+        Frase novaFrase = JsonSerializer.Deserialize<Frase>(frase);
+        File.WriteAllText("fraseGenial.txt", novaFrase.Citacao);
+        Console.WriteLine($"Arquivo Gerado com Sucesso em: {Path.GetFullPath("fraseGenial.txt")}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+    }
+}
